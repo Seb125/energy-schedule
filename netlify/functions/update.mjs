52ae8df6afa1b188496627a2bb6de90dd4cb6586
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-02-28 22:04:54 +0100

    kk

---
+++ netlify/functions/update.mjs
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export default async () => {
+    axios.post("https://energy-guide.adaptable.app/dataupdate")
+    console.log("Data Update Triggered!")
+
+    return new Response("Ok");
+}
+
+export const config = {
+    schedule: "@hourly"
+}
