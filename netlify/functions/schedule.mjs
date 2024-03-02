c4aacfde9824a66582fb5fc7ae4186ea55945dd0
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-03-02 08:43:32 +0100

    using integrated mongodb

--- netlify/functions/schedule.mjs
+++ netlify/functions/schedule.mjs
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default async () => {
-    axios.get("https://energy-guide.adaptable.app/sendmail")
+    axios.get("https://cleaner-tomorrow.adaptable.app/sendmail")
     console.log("Data Update Triggered!")
 
     return new Response("Ok");
