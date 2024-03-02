16f8b47d7e177ccc7af7616f4637036cc620a5b5
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-03-02 09:14:33 +0100

    await

--- netlify/functions/schedule.mjs
+++ netlify/functions/schedule.mjs
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default async () => {
-    axios.get("https://cleaner-tomorrow.adaptable.app/sendmail")
+    await axios.get("https://cleaner-tomorrow.adaptable.app/sendmail")
     console.log("Data Update Triggered!")
 
     return new Response("Ok");
