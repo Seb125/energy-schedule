be0d42b2729adcdc7e6a774974646fe4a2fa132b
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-03-19 14:38:16 +0100

    emailjs

--- netlify/functions/schedule.mjs
+++ netlify/functions/schedule.mjs
@@ -1,12 +1,45 @@
 import axios from "axios";
+import emailjs from "@emailjs/browser";
 
 export default async () => {
-    await axios.get("https://cleaner-tomorrow.adaptable.app/sendmail")
-    console.log("Data Update Triggered!")
+  try {
+    const data = await axios.get("http://localhost:3000/emailjs");
+    emailjs.init(import.meta.env.VITE_EMAIL_USER_ID);
 
-    return new Response("Ok");
-}
+    // Send email with CC included
+    const response = await emailjs.send(
+      import.meta.env.VITE_EMAIL_SERVICE_ID,
+      import.meta.env.VITE_EMAIL_TEMPLATE_ID,
+      data.data.messages[0]
+    );
+
+    const responseTwo = await emailjs.send(
+      import.meta.env.VITE_EMAIL_SERVICE_ID,
+      import.meta.env.VITE_EMAIL_TEMPLATE_ID,
+      data.data.messages[1]
+    );
+
+    const responseThree = await emailjs.send(
+      import.meta.env.VITE_EMAIL_SERVICE_ID,
+      import.meta.env.VITE_EMAIL_TEMPLATE_ID,
+      data.data.messages[2]
+    );
+
+    const responseFour = await emailjs.send(
+      import.meta.env.VITE_EMAIL_SERVICE_ID,
+      import.meta.env.VITE_EMAIL_TEMPLATE_ID,
+      data.data.messages[3]
+    );
+
+    console.log(response);
+    res.json({ message: "Email sent successfully" });
+  } catch (error) {
+    console.log(error);
+  }
+
+  return new Response("Ok");
+};
 
 export const config = {
-    schedule: "@daily"
-}
+  schedule: "@hourly",
+};
