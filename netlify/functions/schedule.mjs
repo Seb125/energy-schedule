18f7473f1198fb60f1342eaa765e27fc020ce588
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-03-19 15:09:54 +0100

    kk

--- netlify/functions/schedule.mjs
+++ netlify/functions/schedule.mjs
@@ -3,36 +3,8 @@ import emailjs from "@emailjs/browser";
 
 export default async () => {
   try {
-    const data = await axios.get("http://localhost:3000/emailjs");
-    emailjs.init(import.meta.env.VITE_EMAIL_USER_ID);
-
-    // Send email with CC included
-    const response = await emailjs.send(
-      import.meta.env.VITE_EMAIL_SERVICE_ID,
-      import.meta.env.VITE_EMAIL_TEMPLATE_ID,
-      data.data.messages[0]
-    );
-
-    const responseTwo = await emailjs.send(
-      import.meta.env.VITE_EMAIL_SERVICE_ID,
-      import.meta.env.VITE_EMAIL_TEMPLATE_ID,
-      data.data.messages[1]
-    );
-
-    const responseThree = await emailjs.send(
-      import.meta.env.VITE_EMAIL_SERVICE_ID,
-      import.meta.env.VITE_EMAIL_TEMPLATE_ID,
-      data.data.messages[2]
-    );
-
-    const responseFour = await emailjs.send(
-      import.meta.env.VITE_EMAIL_SERVICE_ID,
-      import.meta.env.VITE_EMAIL_TEMPLATE_ID,
-      data.data.messages[3]
-    );
-
-    console.log(response);
-    res.json({ message: "Email sent successfully" });
+    axios.get("https://cleaner-tomorrow.adaptable.app/sendmail")
+    console.log("Data Update Triggered!")
   } catch (error) {
     console.log(error);
   }
